c01a2b80e66181e6d2eeabbcfb171a19859299d1
ansdud8239 <ansdud8239@naver.com>
2023-03-09 15:22:57 +0900

    kanbanboard #3

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './assets/scss/Card.scss';
+import TaskList from './TaskList';
 
-const Card = ({no, title, description}) => {
+const Card = ({no, title, description,tasks}) => {
     return (
         <div className={styles.Card}>
             <div className={styles.Card__Title}>{title}</div>
             <div className={styles.Card__Details}>
                 {description}
+                <TaskList tasks={tasks}/>
             </div>  
 
         </div>
