1654f404465d9629b6f463e653b6ee9708b8e453
ansdud8239 <ansdud8239@naver.com>
2023-03-09 16:12:50 +0900

    emaillist #end

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import './assets/css/App.css';
+import RegisterForm from './RegisterForm';
+import Emaillist from './Emaillist';
+import Searchbar from './Searchbar';
+import emails from './assets/json/data.json'
 
-
-function App(props) {
-
+const App = () => {
+    //<Emaillist key={'To Do'} title={'To Do'} cards={cards.filter(card => card.status==='ToDo')}/>
     return (
         <div id='App'>
-            <h1>EmailList</h1>
+            <RegisterForm/>
+            <Searchbar/>
+            <Emaillist emails={emails}/>
         </div>
     );
-}
+};
 
 export default App;
