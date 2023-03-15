ea62252ed054494fcfa860509ea56950d7b15a30
ansdud8239 <ansdud8239@naver.com>
2023-03-15 11:22:19 +0900

    kanbanboard fetch

--- kanbanboard/frontend/config/webpack.config.js
+++ kanbanboard/frontend/config/webpack.config.js
@@ -39,7 +39,13 @@ module.exports = function(env){
             port: 9090,
             liveReload: true,
             compress: true,
-            hot: false 
+            hot: false,
+            proxy: {
+                '/api/': { // /api/로 시작하는 url은 아래의 전체 도메인을 추가하고, 옵션을 적용 
+                  target: 'http://localhost:8080', // 클라이언트에서 api로 보내는 요청은 주소를 3095로 바꿔서 보내겠다 라는 뜻
+                  changeOrigin: true, // cross origin 허용 설정
+              }
+            }
         }
     }
 }
