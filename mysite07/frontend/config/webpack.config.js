75b35ba67ee3b8ef1af9f052315fd2371166c10c
ansdud8239 <ansdud8239@naver.com>
2023-03-20 16:15:30 +0900

    mysite07

---
+++ mysite07/frontend/config/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
+module.exports = function(env){
+    return {
+        mode: "none",
+        entry: path.resolve(`src/index.js`),
+        output: {
+            path: path.resolve('../backend/src/main/resources'),
+            filename: 'assets/js/main.js',
+            assetModuleFilename:'assets/images/[hash][ext]'
+        },
+        module: {
+            rules:[{
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    configFile: path.resolve('config/babel.config.json')
+                }
+            },{
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },
+                    'sass-loader']
+            }, {
+                test: /\.(png|gif|jpe?g|svg|ico|tiff?|bmp)$/i,
+                type: 'asset/resource'
+            }]
+        },
+        plugins: [
+            new CaseSensitivePathsPlugin()
+        ],
+        devtool: "eval-source-map",
+        devServer: {
+            host: '0.0.0.0',
+            port: 9090,
+            liveReload: true,
+            compress: true,
+            hot: false,
+            proxy: {
+                '/api/': { // /api/로 시작하는 url은 아래의 전체 도메인을 추가하고, 옵션을 적용 
+                  target: 'http://localhost:8080', // 클라이언트에서 api로 보내는 요청은 주소를 3095로 바꿔서 보내겠다 라는 뜻
+                  changeOrigin: true, // cross origin 허용 설정
+              }
+            }
+        }
+    }
+}
